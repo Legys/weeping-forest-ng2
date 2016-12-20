f8139776e655e852b1fb96c98414cbcd227a73c5
Alexandr Dubrovin <legys5@yandex.ru>
2016-12-20 20:10:52 +0200

    change server

---
+++ server.js
@@ -0,0 +1,20 @@
+/**
+ * Created by legys on 20.12.2016.
+ */
+"use strict";
+const express = require('express'),
+    app = express(),
+    morgan = require('morgan'),
+    path = require('path'),
+    portNumber = process.env.PORT || process.argv[2] || 8080;
+
+app.use(express.static(__dirname));
+app.use(morgan('dev'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'src/index.html'));
+});
+
+app.listen(portNumber, function () {
+    console.log("Listening on port " + portNumber);
+});
